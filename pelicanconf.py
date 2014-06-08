18663557a188df5f47c35b653cd305032968ff87
DoubleMalt <christoph@web.crofting.com>
2014-06-09 00:22:23 +0200

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Christoph Witzany'
+SITENAME = u'Distinguishable from Magic'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Twitter', 'https://twitter.com/DoubleMalt'),
+          ('Github', 'https://github.com/DoubleMalt'),)
+
+THEME = u'pelican-distiguishable-from-magic'
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
